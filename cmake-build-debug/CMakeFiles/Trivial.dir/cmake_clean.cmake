4bc16095fa9c65a35064268c2e05af1cc99cfd08
alexbs01 <alex.becerra.suarez01@gmail.com>
2022-04-08 14:05:52 +0200

    Proyecto y ramas creadas

---
+++ cmake-build-debug/CMakeFiles/Trivial.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Trivial.dir/main.c.obj"
+  "Trivial.exe"
+  "Trivial.exe.manifest"
+  "Trivial.pdb"
+  "libTrivial.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Trivial.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
