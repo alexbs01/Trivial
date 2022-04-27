d81998e3e4f11678916636bf56ecd17579e9da14
alexbs01 <alex.becerra.suarez01@gmail.com>
2022-04-27 14:02:52 +0200

    Lectura del csv realizado, 30 preguntas añadidas

--- main.c
+++ main.c
@@ -1,6 +1,46 @@
 #include <stdio.h>
+#include <string.h>
+
+#define BUFFER_SIZE 1024
 
 int main() {
-    printf("Hello, World!\n");
+    FILE *ficheroPreguntas;
+    char nombreFicheroPreguntas[] = "preguntas.csv";
+    char buffer[BUFFER_SIZE];
+    char *identificador;
+    enum categoria {ciencias, historia, artes, deportes, ocio, geografia} *categoria;
+    char *pregunta, *opcion1, *opcion2, *opcion3, *opcion4, *opcionCorrecta;
+
+    ficheroPreguntas = fopen(nombreFicheroPreguntas, "r"); // Abre el fichero en modo lectura
+
+    if(ficheroPreguntas == NULL) { // Si no lo puede abrir muestra el mensaje de error y se termina la ejecucion
+        printf("\nNo se pudo abrir el fichero");
+        return 1;
+    }
+
+    const char delimitador[] = ";";
+
+
+    while(fgets(buffer, BUFFER_SIZE, ficheroPreguntas)) {
+
+        identificador = strtok(buffer, delimitador);
+        categoria = (enum categoria *) strtok(NULL, delimitador);
+        pregunta = strtok(NULL, delimitador);
+        opcion1 = strtok(NULL, delimitador);
+        opcion2 = strtok(NULL, delimitador);
+        opcion3 = strtok(NULL, delimitador);
+        opcion4 = strtok(NULL, delimitador);
+        opcionCorrecta = strtok(NULL, delimitador);
+
+        printf("\nIdentificador: %s"
+               "\nCategoria: %s"
+               "\nPregunta: %s"
+               "\nopcion1: %s"
+               "\nopcion2: %s"
+               "\nopcion3: %s"
+               "\nopcion4: %s"
+               "\nopcionCorrecta: %s", identificador, (char*) categoria, pregunta, opcion1, opcion2, opcion3, opcion4, opcionCorrecta);
+    }
+
     return 0;
 }
